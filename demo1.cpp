23923765b54a519713fbe3934477d2efe39f4744
tacy van <fanzitian118@outlook.com>
2023-03-21 12:58:18 +0800

    git commit -a

--- demo1.cpp
+++ demo1.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 // main() 是程序开始执行的地方
 
 int main()
 {
-    
-    wchar_t myStr[] = L"Hello World!";
-    
-    wcout << myStr << endl;
+    double xiebian = 52;
+    double sin_result = xiebian * sin(20); // 计算正弦值
+    double cos_result = xiebian * cos(20); // 计算余弦值
+
+    wcout << sin_result << endl << cos_result << endl;
 
     return 0;
 }

67eeafb3007173a36409e676a476ffde68a2dd43
tacy van <fanzitian118@outlook.com>
2023-03-21 10:00:10 +0800

    On branch master
    Initial commit
    
    Changes to be committed:
    	new file:   .idea/.gitignore
    	new file:   .idea/demo1.iml
    	new file:   .idea/encodings.xml
    	new file:   .idea/modules.xml
    	new file:   .vscode/c_cpp_properties.json
    	new file:   .vscode/launch.json
    	new file:   .vscode/settings.json
    	new file:   .vscode/tasks.json
    	new file:   build/Debug/demo1.o
    	new file:   build/Debug/outDebug.exe
    	new file:   demo1.cpp
    	new file:   demo1.exe
    
    Changes not staged for commit:
    	modified:   demo1.exe
    
    Untracked files:
    	README.md

---
+++ demo1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// main() 是程序开始执行的地方
+
+int main()
+{
+    int a = 10;
+    char ch = 'A';
+    wchar_t myStr[] = L"Hello World!";
+    
+    wcout << myStr << endl;
+
+    return 0;
+}
